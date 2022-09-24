0c830b9df6746d78e3a524aabb0ca31840168dd2
ashrqtt <ashrqtelmahdy@yahoo.com>
2022-09-24 16:47:49 +0200

    initial commit

---
+++ API/Controllers/ProductController.cs
@@ -0,0 +1,31 @@
+﻿using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly StoreDBContext _storeDBContext;
+        public ProductController(StoreDBContext storeDBContext)
+        {
+            _storeDBContext = storeDBContext;
+        }
+        [HttpGet]
+        public async Task<ActionResult<List<Product>>> getProducts()
+        {
+            var products = await _storeDBContext.products.ToListAsync();
+            return Ok(products);
+        }
+
+        [HttpGet("{id}")]//parameter
+        public async Task <ActionResult<Product>> getProductById(int id)
+        {
+            return await _storeDBContext.products.FindAsync(id);
+        }
+    }
+}
---
+++ Infrastructure/Data/StoreDBContext.cs
@@ -0,0 +1,17 @@
+﻿using Core.Entities;
+using Microsoft.EntityFrameworkCore;
+namespace Infrastructure.Data
+{
+    public class StoreDBContext:DbContext
+    {//option "connection string"
+
+        //constructor
+        public StoreDBContext(DbContextOptions<StoreDBContext> options ):base(options)
+        {
+
+        }
+
+        //properties
+        public DbSet<Product>products { get; set; }
+    }
+}
